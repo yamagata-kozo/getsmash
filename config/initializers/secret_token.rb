c1a6952857f5df9ca713e0966f3696580761c722
kozo yamagata <yamagata.kozo@classmethod.jp>
2013-04-22 11:04:46 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getsmash::Application.config.secret_token = 'c229d104c3a252e46a0ee0944ba0d9e291271e49f1cf4f0cb4544bfc8528d96559c884b8b8e9454978c11f65c42aa8435eb51921cc25378b76e7deae18f83c1d'
