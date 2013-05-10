f128403d39aca1fbc1414c5c65d1eb414818fccf
kozo yamagata <yamagata.kozo@classmethod.jp>
2013-05-10 13:09:15 +0900

    ArticleモデルとArticleControllerを作成してViewにデータを表示させるところまで作った

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Article.count == 0
+  10.times do |n|
+    Article.create({
+      title: "Sample Title #{ n }",
+      body: "this is sample article #{ n }"
+    })
+  end
+end
