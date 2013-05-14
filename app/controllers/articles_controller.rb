862b9836653a2b8140d6d59403e7f9b6e29afcd2
kozo yamagata <yamagata.kozo@classmethod.jp>
2013-05-14 13:15:26 +0900

    articleデータをブラウザから作成・更新・削除できる仕組みを実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
@@ -5,5 +6,39 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if false
+      redirect_to @article, notice: '新しいarticleを作成しました'
+    else
+      flash[:alert] = 'articleを作成出来ませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      redirect_to @article, notice: '更新しました'
+    else
+      flash[:alert] = '更新出来ませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: '削除しました'
   end
 end
